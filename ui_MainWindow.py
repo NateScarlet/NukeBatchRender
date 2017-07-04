388bbb250ead235e215f16e8779ad0a73533cc86
NateScarlet <NateScarlet@Gmail.com>
2017-07-04 15:00:37 +0800

    批量渲染添加停止按钮

--- ui_MainWindow.py
+++ ui_MainWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MainWindow.ui'
 #
-# Created: Mon Jul 03 18:19:02 2017
+# Created: Tue Jul 04 13:36:34 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -19,11 +19,25 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout_3 = QtGui.QVBoxLayout()
         self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.horizontalLayout_2 = QtGui.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.formLayout_2 = QtGui.QFormLayout()
+        self.formLayout_2.setObjectName("formLayout_2")
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setObjectName("label_3")
+        self.formLayout_2.setWidget(0, QtGui.QFormLayout.LabelRole, self.label_3)
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.nukeEdit = QtGui.QLineEdit(self.centralwidget)
+        self.nukeEdit.setObjectName("nukeEdit")
+        self.horizontalLayout_4.addWidget(self.nukeEdit)
+        self.nukeButton = QtGui.QPushButton(self.centralwidget)
+        self.nukeButton.setObjectName("nukeButton")
+        self.horizontalLayout_4.addWidget(self.nukeButton)
+        self.formLayout_2.setLayout(0, QtGui.QFormLayout.FieldRole, self.horizontalLayout_4)
         self.label = QtGui.QLabel(self.centralwidget)
         self.label.setObjectName("label")
-        self.horizontalLayout_2.addWidget(self.label)
+        self.formLayout_2.setWidget(1, QtGui.QFormLayout.LabelRole, self.label)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.dirEdit = QtGui.QLineEdit(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -35,7 +49,8 @@ class Ui_MainWindow(object):
         self.dirButton = QtGui.QPushButton(self.centralwidget)
         self.dirButton.setObjectName("dirButton")
         self.horizontalLayout_2.addWidget(self.dirButton)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+        self.formLayout_2.setLayout(1, QtGui.QFormLayout.FieldRole, self.horizontalLayout_2)
+        self.verticalLayout_3.addLayout(self.formLayout_2)
         self.listWidget = QtGui.QListWidget(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -79,6 +94,10 @@ class Ui_MainWindow(object):
         self.renderButton.setMinimumSize(QtCore.QSize(200, 100))
         self.renderButton.setObjectName("renderButton")
         self.verticalLayout_2.addWidget(self.renderButton)
+        self.stopButton = QtGui.QPushButton(self.centralwidget)
+        self.stopButton.setEnabled(False)
+        self.stopButton.setObjectName("stopButton")
+        self.verticalLayout_2.addWidget(self.stopButton)
         self.horizontalLayout.addLayout(self.verticalLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
@@ -94,14 +113,20 @@ class Ui_MainWindow(object):
         self.actionDir.setObjectName("actionDir")
         self.actionStop = QtGui.QAction(MainWindow)
         self.actionStop.setObjectName("actionStop")
+        self.actionNuke = QtGui.QAction(MainWindow)
+        self.actionNuke.setObjectName("actionNuke")
 
         self.retranslateUi(MainWindow)
         QtCore.QObject.connect(self.renderButton, QtCore.SIGNAL("clicked()"), self.actionRender.trigger)
         QtCore.QObject.connect(self.dirButton, QtCore.SIGNAL("clicked()"), self.actionDir.trigger)
+        QtCore.QObject.connect(self.nukeButton, QtCore.SIGNAL("clicked()"), self.actionNuke.trigger)
+        QtCore.QObject.connect(self.stopButton, QtCore.SIGNAL("clicked()"), self.actionStop.trigger)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Nuke批渲染", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Nuke", None, QtGui.QApplication.UnicodeUTF8))
+        self.nukeButton.setText(QtGui.QApplication.translate("MainWindow", "浏览", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MainWindow", "工作目录", None, QtGui.QApplication.UnicodeUTF8))
         self.dirButton.setText(QtGui.QApplication.translate("MainWindow", "浏览", None, QtGui.QApplication.UnicodeUTF8))
         self.versionLabel.setText(QtGui.QApplication.translate("MainWindow", "version", None, QtGui.QApplication.UnicodeUTF8))
@@ -110,7 +135,9 @@ class Ui_MainWindow(object):
         self.priorityCheck.setText(QtGui.QApplication.translate("MainWindow", "低优先级", None, QtGui.QApplication.UnicodeUTF8))
         self.hiberCheck.setText(QtGui.QApplication.translate("MainWindow", "完成后休眠", None, QtGui.QApplication.UnicodeUTF8))
         self.renderButton.setText(QtGui.QApplication.translate("MainWindow", "渲染", None, QtGui.QApplication.UnicodeUTF8))
+        self.stopButton.setText(QtGui.QApplication.translate("MainWindow", "停止渲染", None, QtGui.QApplication.UnicodeUTF8))
         self.actionRender.setText(QtGui.QApplication.translate("MainWindow", "render", None, QtGui.QApplication.UnicodeUTF8))
         self.actionDir.setText(QtGui.QApplication.translate("MainWindow", "dir", None, QtGui.QApplication.UnicodeUTF8))
         self.actionStop.setText(QtGui.QApplication.translate("MainWindow", "stop", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionNuke.setText(QtGui.QApplication.translate("MainWindow", "nuke", None, QtGui.QApplication.UnicodeUTF8))
 

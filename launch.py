b47774725108cb275deb5b2f9e09797bbf860ba2
NateScarlet <NateScarlet@Gmail.com>
2018-06-02 01:38:44 +0800

    Use `win_unicode_console`

--- launch.py
+++ launch.py
@@ -4,7 +4,12 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import sys
+
 import batchrender.__main__
 
 if __name__ == '__main__':
+    if sys.platform == 'win32':
+        import win_unicode_console
+        win_unicode_console.enable()
     batchrender.__main__.main()
